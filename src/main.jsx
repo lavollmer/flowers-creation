867205f383c559a6c469cfb4485a794c19b8d153
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-27 19:12:13 -0600

    updating the main.jsx with correct chakra ui input

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
-
 import * as React from "react";
-
-// 1. import `ChakraProvider` component
 import { ChakraProvider } from "@chakra-ui/react";
+import * as ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <ChakraProvider>
-    <React.StrictMode>
+const rootElement = document.getElementById("root");
+ReactDOM.createRoot(rootElement).render(
+  <React.StrictMode>
+    <ChakraProvider>
       <App />
-    </React.StrictMode>
-  </ChakraProvider>
+    </ChakraProvider>
+  </React.StrictMode>
 );
