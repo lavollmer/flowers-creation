07fb6d78eea3b5a145d6ec55790dbc1369a9bbc3
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-31 13:24:01 -0600

    adding Footer component

--- README.md
+++ README.md
@@ -9,7 +9,19 @@
 Install Vite: https://vitejs.dev/guide/
 Install TailwindCSS with Vite: https://tailwindcss.com/docs/guides/vite
 
+## Technical Stack
+
+This project used:
+
+- Vite
+- React
+- JavaScript
+- TailwindCSS
+- MongoDB
+
 ## Credits
+
+Header component styling inspired by Tailblocks: https://tailblocks.cc/
 
 ## License
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import { useState } from "react";
 //import Main component page
 import Main from "./components/Main";
+//import Header component page
+import Header from "./components/Header";
+//import Footer component page
+import Footer from "./components/Footer";
 import "./App.css";
 
 function App() {
   return (
     <>
+      <Header />
       <Main />
+      <Footer />
     </>
   );
 }
