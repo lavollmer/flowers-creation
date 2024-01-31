32b6351b33844b4a9105ec44460b01fee73a3370
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-01-31 15:26:22 -0600

    debugged tailblocks

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -101,9 +101,7 @@ const Footer = () => {
                     id="footer-field"
                     name="footer-field"
                     className="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:bg-transparent focus:ring-2 focus:ring-indigo-200 focus:border-indigo-500 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-                  >
-                    {" "}
-                  </input>
+                  />
                 </div>
                 <button className="lg:mt-2 xl:mt-0 flex-shrink-0 inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded">
                   Button
@@ -111,7 +109,7 @@ const Footer = () => {
               </div>
               <p className="text-gray-500 text-sm mt-2 md:text-left text-center">
                 Bitters chicharrones fanny pack
-                <br className="lg:block hidden">waistcoat green juice </br>
+                {/* <br className="lg:block hidden">waistcoat green juice </br> */}
               </p>
             </div>
           </div>
