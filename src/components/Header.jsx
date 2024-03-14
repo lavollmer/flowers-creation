f1e5d93ff05394fcf6b0997b45aa6e2d50c1a598
lavollmer <141365306+lavollmer@users.noreply.github.com>
2024-03-14 02:07:11 -0500

    working on reusable instagram logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { GiFlowerPot } from "react-icons/gi";
+import Instagram from "./reusable/instagram";
 
 const Header = () => {
   // Header component styling taken from Tailblocks
@@ -15,25 +16,14 @@ const Header = () => {
             <span className="ml-3 text-xl">Blooming Creations</span>
           </a>
           <nav className="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-            <a className="mr-5 hover:text-gray-900">First Link</a>
-            <a className="mr-5 hover:text-gray-900">Second Link</a>
-            <a className="mr-5 hover:text-gray-900">Third Link</a>
-            <a className="mr-5 hover:text-gray-900">Fourth Link</a>
+            <a className="mr-5 hover:text-gray-900">Products</a>
+            <a className="mr-5 hover:text-gray-900">Design</a>
+            <a className="mr-5 hover:text-gray-900">About Us</a>
+            <a className="mr-5 hover:text-gray-900">FAQ</a>
+            <a className="mr-5 hover:text-gray-900">Contact Us</a>
+            <a className="mr-5 hover:text-gray-900">Blog</a>
           </nav>
-          <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-            Button
-            <svg
-              fill="none"
-              stroke="currentColor"
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              className="w-4 h-4 ml-1"
-              viewBox="0 0 24 24"
-            >
-              <path d="M5 12h14M12 5l7 7-7 7"></path>
-            </svg>
-          </button>
+          <Instagram />
         </div>
       </header>
     </div>
---
+++ src/components/reusable/Instagram.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { FaInstagram } from "react-icons/fa";
+
+const instagram = () => {
+  return (
+    <div>
+      <a
+        href="https://www.instagram.com/sem/campaign/signup/?campaign_id=21000400443&extra_1=s%7Cc%7C690321155980%7Ce%7Cinstagram%20%27%7C&placement&creative=690321155980&keyword=instagram%20%27&partner_id=googlesem&extra_2=campaignid%3D21000400443%26adgroupid%3D157349471966%26matchtype%3De%26network%3Dg%26source%3Dnotmobile%26search_or_content%3Ds%26device%3Dc%26devicemodel%3D%26adposition%3D%26target%3D%26targetid%3Dkwd-1321618851291%26loc_physical_ms%3D9019669%26loc_interest_ms%3D%26feeditemid%3D%26param1%3D%26param2%3D&gad_source=1&gclid=Cj0KCQjwwMqvBhCtARIsAIXsZpY50QnbGuUihLIaa3lmKJQ_jm-bdAYceeF0qLMI4RKnMA9jwb9UFrcaApYtEALw_wcB"
+        alt="Instagram Page"
+      >
+        <FaInstagram size={25} />
+      </a>
+    </div>
+  );
+};
+
+export default instagram;
