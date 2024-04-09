3f3c05b7fbdfa36878b473df21ac805b24bce1ed
Laura <laura.e.vollmer@gmail.com>
2024-04-09 11:32:45 -0500

    figure out routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useState } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 //import Header component page
 import Header from "./components/Header";
 //import Footer component page
