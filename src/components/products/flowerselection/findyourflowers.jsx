56b45f207f1b16a0fadb35de3e8b0dde3281ebd1
Laura <laura.e.vollmer@gmail.com>
2024-04-09 11:46:46 -0500

    creating sections

---
+++ src/components/products/flowerselection/findyourflowers.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const findyourflowers = () => {
+  return (
+    <div>findyourflowers</div>
+  )
+}
+
+export default findyourflowers
---
+++ src/components/products/flowerselection/popularflowers.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const popularflowers = () => {
+  return (
+    <div>popularflowers</div>
+  )
+}
+
+export default popularflowers
